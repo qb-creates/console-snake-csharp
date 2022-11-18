6f4cfbe4174baea8aabc9084d41c3020a2f53a7c
qbaker-wayahealth <qbaker@wayahealth.com>
2022-11-18 01:00:17 -0500

    Uploading Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
